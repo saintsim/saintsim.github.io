51b44967ceffa1da583e6dfed00c9c566bfe6e19
simon.scarpa <simon.scarpa@gmail.com>
2025-01-19 16:09:40 +0900

    Use plain JS to get weather

---
+++ built/weather-simple.js
@@ -0,0 +1,20 @@
+"use strict";
+function getWeatherData() {
+    var xhr = new XMLHttpRequest();
+    var url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+            // You can process the weather data here
+            document.getElementById("weather").textContent = "Temperature: " +
+                response.hourly.temperature_2m[0] + "°C";
+        }
+        else if (xhr.readyState === 4) {
+            console.error("Failed to fetch weather data.");
+        }
+    };
+    xhr.send();
+}
+getWeatherData();
