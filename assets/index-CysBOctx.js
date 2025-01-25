42e67da8ec4b0911300e51676561613ecb5362ee
saintsim <saintsim@users.noreply.github.com>
2025-01-25 16:45:44 +0000

    deploy: 83e0b4fe669923733801992cbdc9cebbe443ecce

--- assets/index-CysBOctx.js
+++ assets/index-CysBOctx.js
@@ -319,8 +319,6 @@ const morningBlockName = "Morning";
 const afternoonBlockName = "Afternoon";
 const eveningBlockName = "Evening";
 let chanceOfRainPerc = 0;
-let isUmbrellaNeeded = false;
-let rainTotalExpected = 0;
 function getHours(blockName) {
     switch (blockName) {
         case morningBlockName:
@@ -385,17 +383,7 @@ function setElementBlock(id, data) {
         currentElement.textContent = data;
     }
 }
-function hideBlock(id) {
-    const currentElement = document.getElementById(id);
-    if (currentElement) {
-        currentElement.style.display = "none";
-    }
-}
 function updateGlobalRainDetails(block) {
-    if (block.precepitationPercHighest > 50) {
-        isUmbrellaNeeded = true;
-        rainTotalExpected += block.totalRainfall;
-    }
     if (chanceOfRainPerc < block.precepitationPercHighest) {
         chanceOfRainPerc = block.precepitationPercHighest;
     }
@@ -432,11 +420,10 @@ function updateChanceOfRainBlock() {
     const chanceOfRainStr = `${chanceOfRainPerc}%`;
     setElementBlock("carryUmbrealla", `${chanceOfRainStr}`);
     const umbrellaIcon = "umbrellaIcon";
-    if (isUmbrellaNeeded) {
-        document.getElementById(umbrellaIcon).src = getUmbrellaIcon(rainTotalExpected > 10);
-    }
-    else {
-        hideBlock(umbrellaIcon);
+    //if (isUmbrellaNeeded) {
+    {
+        //(document.getElementById(umbrellaIcon) as HTMLImageElement).src = getUmbrellaIcon(rainTotalExpected > 10);
+        document.getElementById(umbrellaIcon).src = getUmbrellaIcon(true);
     }
 }
 function updatePage(pageResponse) {
