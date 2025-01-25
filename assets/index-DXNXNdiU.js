d59e19d0a9a092ff97f0d20aa3254495202a9f63
saintsim <saintsim@users.noreply.github.com>
2025-01-25 05:25:22 +0000

    deploy: a959032f7b066882b38e7b653ff4c369cb8e9742

--- assets/index-DXNXNdiU.js
+++ assets/index-DXNXNdiU.js
@@ -129,6 +129,68 @@ function getWeatherImage(weatherCode) {
         }
     }
 }
+function getWeatherDescription(weatherCode) {
+    switch (weatherCode) {
+        case 0:
+            return "Clear sky";
+        case 1:
+            return "Mainly clear";
+        case 2:
+            return "Partly cloudy";
+        case 3:
+            return "Overcast";
+        case 45:
+            return "Fog";
+        case 48:
+            return "Depositing rime fog";
+        case 51:
+            return "Drizzle: Light";
+        case 53:
+            return "Drizzle: Moderate";
+        case 55:
+            return "Drizzle: Dense intensity";
+        case 56:
+            return "Freezing Drizzle: Light";
+        case 57:
+            return "Freezing Drizzle: Dense intensity";
+        case 61:
+            return "Rain: Slight";
+        case 63:
+            return "Rain: Moderate";
+        case 65:
+            return "Rain: Heavy intensity";
+        case 66:
+            return "Freezing Rain: Light";
+        case 67:
+            return "Freezing Rain: Heavy intensity";
+        case 71:
+            return "Snow fall: Slight";
+        case 73:
+            return "Snow fall: Moderate";
+        case 75:
+            return "Snow fall: Heavy intensity";
+        case 77:
+            return "Snow grains";
+        case 80:
+            return "Rain showers: Slight";
+        case 81:
+            return "Rain showers: Moderate";
+        case 82:
+            return "Rain showers: Violent";
+        case 85:
+            return "Snow showers: Slight";
+        case 86:
+            return "Snow showers: Heavy";
+        case 95:
+            return "Thunderstorm: Slight or moderate";
+        case 96:
+            return "Thunderstorm with slight and heavy hail";
+        case 99:
+            return "Thunderstorm: Heavy hail";
+        default:
+            return "Unknown";
+    }
+}
 function getTempBlock(blockName) {
     return {
         blockName: blockName,
@@ -144,25 +206,30 @@ function getTempBlock(blockName) {
         weatherCode: 0
     };
 }
+function getCurrentChanceOfRain(percentageChance) {
+    if (percentageChance === 0)
+        return "No rain currently";
+    else {
+        return `${percentageChance}% chance of rain`;
+    }
+}
+function setElementBlock(id, data) {
+    const currentElement = document.getElementById(id);
+    if (currentElement) {
+        currentElement.textContent = data;
+    }
+}
 function updatePage(pageResponse) {
-    //const temperature = new WeatherData(xhr.responseText).getTemperature();
     const response = JSON.parse(pageResponse);
     console.log(response);
-    const temperature = `Temperature: ${response.hourly.temperature_2m[0]}°C`;
-    console.log(temperature); // Log or set the temperature
-    const weatherElement = document.getElementById("weather");
-    if (weatherElement) {
-        weatherElement.textContent = temperature;
-    }
-    const weatherCode = response.current_weather.weathercode;
-    const weatherIcon = getWeatherImage(weatherCode);
-    document.getElementById('amWeatherIcon').src = weatherIcon;
+    const weatherCode = response.current.weather_code;
+    setElementBlock("currentTemperature", `${response.current.temperature_2m}°C (${response.current.apparent_temperature}°C)`);
+    setElementBlock("currentConditions", getWeatherDescription(weatherCode));
+    setElementBlock("currentRain", getCurrentChanceOfRain(response.current.rain));
+    document.getElementById('amWeatherIcon').src = getWeatherImage(weatherCode);
     // update the blocks
     const morningBlock = getTempBlock("Morning Block");
-    const blocmMorningElement = document.getElementById("blockMorning");
-    if (blocmMorningElement) {
-        blocmMorningElement.textContent = weatherConfig.blockMorningHours.toString() + morningBlock.blockName;
-    }
+    setElementBlock("blockMorning", weatherConfig.blockMorningHours.toString() + morningBlock.blockName);
     //const afternoonBlock = getTempBlock("Afternoon Block");
     //const eveningBlock = getTempBlock("Evening Block");
 }
@@ -173,8 +240,7 @@ function getWeatherData() {
         "latitude=" + latitude +
         "&longitude=" + longitude +
         "&timezone=Asia%2FTokyo" +
-        "&current_weather=true" +
-        "&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code" +
+        "&current=temperature_2m,apparent_temperature,is_day,precipitation,precipitation_probability,rain,showers,snowfall,weather_code" +
         "&forecast_days=1" +
         "&hourly=temperature_2m,weathercode,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth";
     const xhr = new XMLHttpRequest();
@@ -189,7 +255,9 @@ function getWeatherData() {
             }
         }
         else {
-            console.log("Failed to fetch weather data.");
+            if (xhr.status !== 200) {
+                console.log(`Failed to fetch weather data: ${xhr.status} / ${xhr.readyState}`);
+            }
         }
     };
     xhr.send();
