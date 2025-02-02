ec6ae18832d682ca0a2cc62ba7781162f77227d0
saintsim <saintsim@users.noreply.github.com>
2025-02-02 16:02:36 +0000

    deploy: ed3e4f9bb8edb57044ebd409c08a1da045b3a00c

--- assets/index-DJQRU80b.js
+++ assets/index-DJQRU80b.js
@@ -74,6 +74,12 @@ function scheduleUpdateDate() {
         setInterval(updateDate, 24 * 60 * 60 * 1000); // Update every 24 hours
     }, timeToMidnight);
 }
+// used by the bus module
+function isTodayAWeekday() {
+    let now = new Date();
+    const currentDayOfWeek = daysOfWeek[now.getDay()];
+    return currentDayOfWeek != "Saturday" && currentDayOfWeek !== "Sunday";
+}
 scheduleUpdateDate();
 setInterval(updateTime, 1000); // every 1 second
 
@@ -459,7 +465,7 @@ function getCurrentChanceOfRain(percentageChance, totalRain, now) {
         }
     }
 }
-function setElementBlock(id, data) {
+function setElementBlock$1(id, data) {
     const currentElement = document.getElementById(id);
     if (currentElement) {
         currentElement.textContent = data;
@@ -484,17 +490,17 @@ function updateBlock(blockName, elementName, boxName, iconName, currentHour, res
         const tempMaxFeelsLikeString = `${block.tempFeelsLikeMax}°C`;
         const conditionsString = `${getWeatherDescription(block.weatherCode)}`;
         const percString = `${getCurrentChanceOfRain(block.precepitationPercHighest, block.totalRainfall, false)}`;
-        setElementBlock(elementName + "Title", `${block.blockName}`);
+        setElementBlock$1(elementName + "Title", `${block.blockName}`);
         document.getElementById(iconName).src = getWeatherImage(block.weatherCode);
-        setElementBlock(elementName + "TempMinFeelsLike", `${tempMinFeelsLikeString}`);
-        setElementBlock(elementName + "TempMaxFeelsLike", `${tempMaxFeelsLikeString}`);
-        setElementBlock(elementName + "CurrentConditions", `${conditionsString}`);
-        setElementBlock(elementName + "CurrentRain", `${percString}`);
+        setElementBlock$1(elementName + "TempMinFeelsLike", `${tempMinFeelsLikeString}`);
+        setElementBlock$1(elementName + "TempMaxFeelsLike", `${tempMaxFeelsLikeString}`);
+        setElementBlock$1(elementName + "CurrentConditions", `${conditionsString}`);
+        setElementBlock$1(elementName + "CurrentRain", `${percString}`);
     }
 }
 function updateCurrentBlock(response, weatherCode) {
-    setElementBlock("currentTemperatureFeelsLike", `${Math.round(response.current.apparent_temperature)} °C`);
-    setElementBlock("currentConditions", getWeatherDescription(weatherCode));
+    setElementBlock$1("currentTemperatureFeelsLike", `${Math.round(response.current.apparent_temperature)} °C`);
+    setElementBlock$1("currentConditions", getWeatherDescription(weatherCode));
     document.getElementById('currentWeatherIcon').src = getWeatherImage(weatherCode);
 }
 function updateDayBlocks(response, currentHour) {
@@ -504,7 +510,7 @@ function updateDayBlocks(response, currentHour) {
 }
 function updateChanceOfRainBlock() {
     const chanceOfRainStr = `${chanceOfRainPerc}%`;
-    setElementBlock("carryUmbrealla", `${chanceOfRainStr}`);
+    setElementBlock$1("carryUmbrealla", `${chanceOfRainStr}`);
     const umbrellaIcon = "umbrellaIcon";
     document.getElementById(umbrellaIcon).src = getUmbrellaIcon(chanceOfRainPerc);
 }
@@ -516,8 +522,8 @@ function updatePage(pageResponse) {
     updateCurrentBlock(response, weatherCode);
     updateDayBlocks(response, currentHour);
     updateChanceOfRainBlock();
-    setElementBlock("currentHour", currentHour);
-    setElementBlock("todayRain", getCurrentChanceOfRain(chanceOfRainPerc, rainTotalExpected, true));
+    setElementBlock$1("currentHour", currentHour);
+    setElementBlock$1("todayRain", getCurrentChanceOfRain(chanceOfRainPerc, rainTotalExpected, true));
     updateOutfitOptions(minTemperature, chanceOfRainPerc);
 }
 function getWeatherData() {
@@ -551,3 +557,111 @@ function getWeatherData() {
 }
 getWeatherData();
 setInterval(getWeatherData, 600000); // refresh every 10mins
+
+// dates added on 02Feb2025 (https://tokyo-brt.co.jp/bus-stops/b02-kachidoki-brt/)
+const brtKachidokiToToranomonBusTimes = {
+    6: [31, 51],
+    7: [12, 32, 51],
+    8: [13, 21, 33, 41, 53],
+    9: [13, 33, 54],
+    10: [13, 33, 56],
+    11: [13, 33, 56],
+    12: [13, 33, 54],
+    13: [13, 35, 53],
+    14: [13, 33, 53],
+    15: [13, 33, 57],
+    16: [16, 33, 53]
+    // don't expect to take thus bus after this
+};
+const brtToranomonToKachidokiBusTimes = {
+    // don't expect to use a bus earlier than this
+    12: [15, 33, 53],
+    13: [15, 32, 53],
+    14: [15, 32, 52],
+    15: [15, 32, 52],
+    16: [15, 32, 52],
+    17: [15, 32, 52],
+    18: [13, 23, 35, 45, 55],
+    19: [14, 34, 50],
+    20: [11, 30, 50],
+    21: [10, 50],
+    22: [11]
+};
+function findNextBus(hour, minute, schedule) {
+    const currentTime = new Date();
+    // Convert schedule into sorted array of all times
+    const allBusTimes = Object.entries(schedule).flatMap(([h, minutes]) => {
+        const baseTime = new Date(currentTime);
+        return minutes.map(m => {
+            const busTime = new Date(baseTime);
+            busTime.setHours(parseInt(h), m, 0, 0);
+            return {
+                hour: parseInt(h),
+                minute: m,
+                time: busTime
+            };
+        });
+    }).sort((a, b) => a.time.getTime() - b.time.getTime());
+    // Find the next bus
+    const nextBus = allBusTimes.find(bus => {
+        if (bus.hour > hour)
+            return true;
+        if (bus.hour === hour && bus.minute > minute)
+            return true;
+        return false;
+    });
+    if (!nextBus) {
+        return {
+            nextBus: "No more buses today. Check tomorrow's schedule.",
+            waitTime: 0
+        };
+    }
+    return {
+        nextBus: `${nextBus.hour.toString().padStart(2, '0')}:${nextBus.minute.toString().padStart(2, '0')}`,
+        waitTime: Math.round((nextBus.time.getTime() - currentTime.getTime()) / (1000 * 60)) // in minutes
+    };
+}
+function setElementBlock(id, data) {
+    const currentElement = document.getElementById(id);
+    if (currentElement) {
+        currentElement.textContent = data;
+    }
+}
+function getNextTwoBusTimes() {
+    // check are we a week day
+    if (isTodayAWeekday()) {
+        let now = new Date();
+        const currentHour = now.getHours();
+        const currentMinute = now.getMinutes();
+        let nextBus;
+        let nextNextBus;
+        if (currentHour < 14) {
+            // assumes I'm taking the bus from home
+            nextBus = findNextBus(currentHour, currentMinute, brtKachidokiToToranomonBusTimes);
+            if (nextBus.nextBus.includes(":")) {
+                const busTime = nextBus.nextBus.split(":");
+                nextNextBus = findNextBus(Number(busTime[0]), Number(busTime[1]), brtKachidokiToToranomonBusTimes);
+            }
+            else {
+                nextNextBus = nextBus;
+            }
+            setElementBlock("nextBusRoute", "Next bus to Toranomon:");
+        }
+        else {
+            // assumes I'm taking bus back from work
+            nextBus = findNextBus(currentHour, currentMinute, brtToranomonToKachidokiBusTimes);
+            if (nextBus.nextBus.includes(":")) {
+                const busTime = nextBus.nextBus.split(":");
+                nextNextBus = findNextBus(Number(busTime[0]), Number(busTime[1]), brtToranomonToKachidokiBusTimes);
+            }
+            else {
+                nextNextBus = nextBus;
+            }
+            setElementBlock("nextBusRoute", "Next bus to Kachidoki:");
+        }
+        setElementBlock("nextBus", nextBus.nextBus);
+        setElementBlock("nextNextBus", nextNextBus.nextBus);
+    }
+}
+getNextTwoBusTimes();
+setInterval(getNextTwoBusTimes, 600000); // refresh every 10mins
